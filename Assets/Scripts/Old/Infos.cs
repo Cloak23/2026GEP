3d08c19ba1841ec83e1f5252c0e0f0b3ef87fac3
Cloak23 <44891642+Cloak23@users.noreply.github.com>
2026-05-27 00:50:12 +0900

    안쓰는 스크립트 이동

---
+++ Assets/Scripts/Old/Infos.cs
@@ -0,0 +1,33 @@
+using System;
+
+
+// 더이상 사용 X
+public enum RoomType
+{
+    Empty,
+    Mine,
+    Number,
+    Item
+}
+
+
+// 더이상 사용 X
+public class SlotData
+{
+    int x, y;
+
+    public RoomType type;
+    public int mine_num;
+    public int amount;
+    public bool isVisible;
+
+    public SlotData(int x = -1, int y = -1, RoomType type = RoomType.Empty, int mine_num = 0, int amount = 5, bool isVisible = false)
+    {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.mine_num = mine_num;
+        this.amount = amount;
+        this.isVisible = isVisible;
+    }
+}
